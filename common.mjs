78937e4729fd43e71f4ff46c83f27941dcbfe721
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-10-21 22:06:28 +0100

    Joined arrays

--- common.mjs
+++ common.mjs
@@ -1,3 +1,8 @@
-import { getUserIDs } from "./data.mjs";
+import { getUserIDs, getSong, getListenEvents } from "./data.mjs";
+
+
 
 export const countUsers = () => getUserIDs().length;
+export const getSong2 = (songID) =>  getSong(songID);
+export const getListenEvents2 = (userID) => getListenEvents(userID);
+
--- script.mjs
+++ script.mjs
@@ -4,8 +4,23 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { countUsers } from "./common.mjs";
+import { countUsers, getSong2, getListenEvents2  } from "./common.mjs";
+
+function joinEventAndSongs(userID){
+  const userData= getListenEvents2(userID);
+  const events = Object.values(userData).flat();
+  const joined = events.map(e => {
+  const song = getSong2(e.song_id);
+    return {
+      ...e,       
+      ...song, 
+    };
+  });
+  joined.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+  return joined;
+}
 
 window.onload = function () {
   document.querySelector("body").innerText = `There are ${countUsers()} users`;
+  console.log(joinEventAndSongs(1));
 };
